930c212d2f556274f08fdb863ba62f89e9d9b11b
FlorenciaFried <florencia.d.fried@gmail.com>
2020-07-21 17:27:37 -0300

    Guardando el input de nueva receta en bd y validando el form de nueva receta

--- RedSocialRecetas/app/resources/views/recetas/create.blade.php
+++ RedSocialRecetas/app/resources/views/recetas/create.blade.php
@@ -6,4 +6,29 @@
 
 @section('content')
     <h2 class="text-center mb-5">Crear Nueva Receta</h2>
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form method="POST" action="{{ route('recetas.store') }}" novalidate>
+                @csrf
+
+                <div class="form-group">
+                    <label for="titulo">Título Receta</label>
+
+                    <input type="text" name='titulo' class="form-control @error('titulo') is-invalid @enderror" id="titulo"
+                        placeholder="Titutlo Receta" value="{{ old('titulo') }}" />
+
+                    @error('titulo')
+                    <span class="invalid-feedback d-block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta" />
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
